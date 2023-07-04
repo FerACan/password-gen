d163fc66131374b046f454a98e2a246d1339d297
FerACan <134230590+FerACan@users.noreply.github.com>
2023-07-04 18:35:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 // A function to get the password
     function getPassword() {
     
-// Password variables for allowable passwords characters
     var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var lowercaseABC = "abcdefghijklmnopqrstuvwxyz"
     var numeric = "0123456789"
@@ -12,17 +11,15 @@
     var multiSelect = "";
     var randomKey = "";
 
-//Initial prompt to allow end-user to select a password length between 8 to 128 characters long.
+
     var keyLength = prompt(
         "Your password must be between 8 to 128 characters long, enter your desired number."
     );
     
-// Return to window if the selected number is out of range for password criteria
     if (keyLength < 8 || keyLength > 128 || isNaN(keyLength)) {
         alert("Your password does not meet the critia. Password must be between 8 and 128 characters in length. Enter another number.");        
         return;
 
-// Variable Prompts. Once the end user successfully enters a valid numberor value, this prompt will notify them that their password will contain the following characters when the random key is finally generated
     } else {
     var upperAlpha = confirm("Your password will contain a variety of uppercase letters, click ok to continue");
     var lowerAlpha = confirm("Your password will contain a variety of lowercase letters, click ok to continue");
@@ -35,16 +32,13 @@
     if (numericOptions) {multiSelect += numeric;}
     if (specialSymbols) {multiSelect += symbols;}
 
-// For loop to repeat if criteria is not met and will end once password is generated. 
     for (i = 0; i < keyLength; i++) {
 
-// Final random password has generated
         randomKey += multiSelect[Math.floor(Math.random() * multiSelect.length)]
     }
     return(randomKey);
 }
 
-// Write password to the #password input. The text box area where the newly generated password will appear.
     var passwordText = document.querySelector("#password");
     function writePassword(randomKey) {
     if (password.length === 0) {
@@ -53,5 +47,4 @@
     passwordText.value = randomKey;
 }
 
-// Add event listener to generate button. This is a Call To Action (CTA button)
     generateBtn.addEventListener("click", function () { writePassword(getPassword()) });
